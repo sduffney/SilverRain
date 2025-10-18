2a33d974c91abc90558f978dea91b828435ef5d9
Maria Angélica Cuadrado Rodriguez <angel.kira65@gmail.com>
2025-10-17 21:13:23 -0400

    -

--- SilverRain/Assets/Scripts/Enemy.cs
+++ SilverRain/Assets/Scripts/Enemy.cs
@@ -1,35 +1,35 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    EnemyHealth health;
-    EnemyController controller;
-    [SerializeField] private int scoreValue;
-    [SerializeField] private int xpValue;
-    [SerializeField] private float damage;
-    private Renderer renderer;
+//public class Enemy : MonoBehaviour
+//{
+//    EnemyHealth health;
+//    EnemyController controller;
+//    [SerializeField] private int scoreValue;
+//    [SerializeField] private int xpValue;
+//    [SerializeField] private float damage;
+//    private Renderer renderer;
 
-    private void OnEnable()
-    {
-        //Subscribe to reveal all event
-    }
+//    private void OnEnable()
+//    {
+//        //Subscribe to reveal all event
+//    }
 
-    private void OnDisable()
-    {
-        //Unsubscribe to reveal all event
-    }
+//    private void OnDisable()
+//    {
+//        //Unsubscribe to reveal all event
+//    }
 
-    private void Update()
-    {
-        //Trigger reveal when reveal all event is called
-    }
-    public void Reveal()
-    {
-        Component.renderer.enabled = true;
-    }
+//    private void Update()
+//    {
+//        //Trigger reveal when reveal all event is called
+//    }
+//    public void Reveal()
+//    {
+//        Component.renderer.enabled = true;
+//    }
 
-    public void Hide()
-    {
-        renderer.enabled = false;
-    }
-}
+//    public void Hide()
+//    {
+//        renderer.enabled = false;
+//    }
+//}
